77253c49d74d84c14210a45d949647ee99f162e4
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-09-27 19:37:14 +0200

    Commit changes

--- Program.js
+++ Program.js
@@ -3,14 +3,17 @@ process.stdin.on("readable", function() {
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
-        console.log(process.env);
         switch(instruction) {
             case "/exit":
                 process.stdout.write("Quitting app!\n");
                 process.exit();
                 break;
-            default: 
-            process.stderr.write("Wrong instruction!");
+            case "/info":
+                console.log(process.env.LANG);
+                console.log(process.versions.node);
+           default: 
+            process.stderr.write("Wrong instruction!\n");
         }
     }
 });
+
